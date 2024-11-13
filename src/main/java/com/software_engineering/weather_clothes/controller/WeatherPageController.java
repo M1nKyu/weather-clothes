e3cb4c0ebe525e24ffc1c85ee734c29bc830e82d
eokjae <cjdeh0904@naver.com>
2024-11-13 15:50:12 +0900

    refactor: delete import

--- src/main/java/com/software_engineering/weather_clothes/controller/WeatherPageController.java
+++ src/main/java/com/software_engineering/weather_clothes/controller/WeatherPageController.java
@@ -10,10 +10,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.util.List;
-import java.util.Arrays;
+
 
 @Controller
 public class WeatherPageController {
