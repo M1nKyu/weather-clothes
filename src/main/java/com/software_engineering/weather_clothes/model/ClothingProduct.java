b40639d3041b908b1c04aad95d154170dea6965a
M1nKyu <01.minkyu@gmail.com>
2024-11-14 20:36:43 +0900

    feat: Change crawling method
    - 기존은 랭킹에서 상품을 가져왔지만, 옷이 아닌 제품도 포함되어 있고, HTML에 카테고리 정보는 포함하지 않아 상품 URL로 이동하여 카테고리를 가져오는 과정이 발생함.
    
    - 각 카테고리 ID를 추출하였고 카테고리별 사이트의 주소가 `https://www.musinsa.com/category/카테고리ID?성별정보` 형태임을 확인하였고, category id를 리스트화 하여 페이지를 이동해가며 카테고리별 일정 개수의 상품을 가져오도록 했음.
    
    - clothing_product 데이터베이스 구조도 수정

--- src/main/java/com/software_engineering/weather_clothes/model/ClothingProduct.java
+++ src/main/java/com/software_engineering/weather_clothes/model/ClothingProduct.java
@@ -16,23 +16,11 @@ public class ClothingProduct {
     private String productId;  // 상품 ID (data-item-id)
 
     private String category;   // 상품 카테고리
-
-    private String price;      // 가격
     private String link;       // 상품 상세 링크
-    private String brand;      // 브랜드
     private String imageUrl;   // 이미지 URL
+    private String likes;     // 상품 좋아요 수
 
-    // 기본 생성자
-    public ClothingProduct() {
-    }
+    @Column(name = "last_updated", nullable = false)
+    private String lastUpdated;  // 마지막 갱신 시간 (YYMMDDHHmm)
 
-    // 모든 필드를 포함한 생성자
-    public ClothingProduct(String productId, String price, String link, String brand, String imageUrl, String category) {
-        this.productId = productId;
-        this.price = price;
-        this.link = link;
-        this.brand = brand;
-        this.imageUrl = imageUrl;
-        this.category = category;
-    }
 }
