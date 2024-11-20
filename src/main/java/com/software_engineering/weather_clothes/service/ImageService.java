42ab478b2a0564e2a1572aa0de05a7bdf6cb6dde
M1nKyu <01.minkyu@gmail.com>
2024-11-20 18:44:25 +0900

    refactor: Update selectWeatherIcon method
    - WeatherStatusEnums에 정의된 Enum을 사용하도록 개선

--- src/main/java/com/software_engineering/weather_clothes/service/ImageService.java
+++ src/main/java/com/software_engineering/weather_clothes/service/ImageService.java
@@ -1,5 +1,6 @@
 package com.software_engineering.weather_clothes.service;
 
+import com.software_engineering.weather_clothes.enums.WeatherStatusEnums.*;
 import com.software_engineering.weather_clothes.model.Weather;
 import com.software_engineering.weather_clothes.util.DateTimeUtil;
 import org.springframework.stereotype.Service;
@@ -7,33 +8,44 @@ import org.springframework.stereotype.Service;
 @Service
 public class ImageService {
     public String selectWeatherIcon(Weather weather){
-        int sky = weather.getSky();
-        int pty = weather.getPty();
+        int t1hValue = weather.getT1h();
+        int skyValue = weather.getSky();
+        int ptyValue = weather.getPty();
         boolean isDaytime = DateTimeUtil.isDaytime(weather.getFcstTime());
+
+        SkyCondition sky = SkyCondition.from(skyValue);
+        PrecipitationType pty = PrecipitationType.from(ptyValue);
 
         String icon = "";
 
-        if(pty == 0){
-            if(sky >= 0 && sky <= 5){
-                icon =  "clear";
-            }
-            else if(sky >= 6 && sky <= 8){
-                icon = "partly_cloudy";
-            }
-            else if(sky >= 9 && sky <= 10){
-                icon = "cloudy";
-            }
-            else {
-                icon = "clear";
+        // 강수 형태가 없는 경우
+        if(pty == PrecipitationType.NONE) {
+            switch(sky){
+                case CLEAR -> icon = "clear";
+                case MOSTLY_CLEAR -> icon = "partly-cloudy";
+                case MOSTLY_CLOUDY -> icon = "overcast";
+                case CLOUDY -> icon = "extreme";
             }
         }
-        else if(pty == 1 || pty == 2 || pty == 5 || pty == 6){
+        // 비 또는 비/눈 형태일 경우
+        else if(t1hValue >= 0 && isRainOrRainSnow(pty)) {
             icon = "rain";
         }
-        else if(pty == 3 || pty == 7){
+        // 눈 또는 눈/비 형태일 경우
+        else if(t1hValue < 0 && isSnowOrSnowRain(pty)) {
             icon = "snow";
         }
 
         return "/images/weather-icons/" + (isDaytime ? "day/" : "night/") + icon + ".svg";
+    }
+
+    private boolean isRainOrRainSnow(PrecipitationType pty) {
+        return pty == PrecipitationType.RAIN || pty == PrecipitationType.RAIN_DROP ||
+                pty == PrecipitationType.RAIN_SNOWFLAKE || pty == PrecipitationType.RAIN_SNOW;
+    }
+
+    private boolean isSnowOrSnowRain(PrecipitationType pty) {
+        return pty == PrecipitationType.SNOW || pty == PrecipitationType.SNOWFLAKE ||
+                pty == PrecipitationType.RAIN_SNOWFLAKE;
     }
 }
