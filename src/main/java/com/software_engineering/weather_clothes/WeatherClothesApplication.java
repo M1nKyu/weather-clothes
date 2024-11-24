bc3ed3fba0fe4382d9a76afd67030dc2c49a4011
M1nKyu <01.minkyu@gmail.com>
2024-11-24 21:24:31 +0900

    feat: Add dotenv dependency
    - dotenv 의존성을 추가하여 application.properties에 필요한 정보를 .env파일에 분리하여 저장하도록 변경

--- src/main/java/com/software_engineering/weather_clothes/WeatherClothesApplication.java
+++ src/main/java/com/software_engineering/weather_clothes/WeatherClothesApplication.java
@@ -1,5 +1,6 @@
 package com.software_engineering.weather_clothes;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WeatherClothesApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();  // .env 파일 로드
+		// 환경 변수를 시스템에 등록
+		dotenv.entries().forEach(entry ->
+				System.setProperty(entry.getKey(), entry.getValue())
+		);
 
 		SpringApplication.run(WeatherClothesApplication.class, args);
 	}
