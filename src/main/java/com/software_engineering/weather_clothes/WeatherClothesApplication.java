0f572e0cbe6de7f003aeb3ec83082e2a67a95f21
M1nKyu <124333357+M1nKyu@users.noreply.github.com>
2024-11-25 22:03:04 +0900

    feat: Update WeatherClothesApplication.java for server

--- src/main/java/com/software_engineering/weather_clothes/WeatherClothesApplication.java
+++ src/main/java/com/software_engineering/weather_clothes/WeatherClothesApplication.java
@@ -7,13 +7,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WeatherClothesApplication {
 
-	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load();  // .env 파일 로드
-		// 환경 변수를 시스템에 등록
-		dotenv.entries().forEach(entry ->
-				System.setProperty(entry.getKey(), entry.getValue())
-		);
-
-		SpringApplication.run(WeatherClothesApplication.class, args);
-	}
+        public static void main(String[] args) {
+                Dotenv dotenv = Dotenv.configure()
+                        .directory("/home/ubuntu/weather-clothes")  // .env 파일이 있는 디렉토리 경로를 지정
+                        .load();
+                // 환경 변수를 시스템에 등록
+                dotenv.entries().forEach(entry ->
+                                System.setProperty(entry.getKey(), entry.getValue())
+                );
+		
+                SpringApplication.run(WeatherClothesApplication.class, args);
+        }
 }
