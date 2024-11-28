5d220d3ab72de96ddd9f0b7ea326d4cb148a05c5
M1nKyu <01.minkyu@gmail.com>
2024-11-29 00:48:49 +0900

    refactor: Add TemperatureRange enum
    - 기온의 범위를 enum으로 생성하여 코드 가독성 개선

--- src/main/java/com/software_engineering/weather_clothes/service/ClothingCategoryService.java
+++ src/main/java/com/software_engineering/weather_clothes/service/ClothingCategoryService.java
@@ -7,6 +7,7 @@ import com.software_engineering.weather_clothes.repository.ClothingCategoryRepos
 import com.software_engineering.weather_clothes.repository.ClothingProductRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
+import com.software_engineering.weather_clothes.enums.WeatherStatusEnums.TemperatureRange;
 
 import java.util.*;
 import java.util.stream.Collectors;
@@ -28,168 +29,45 @@ public class ClothingCategoryService {
      * @param weather
      * @return 옷 카테고리별 추천 리스트
      */
-    public Map<String, List<ClothingCategory>> getClothingCategory(Weather weather){
-
+    public Map<String, List<ClothingCategory>> getClothingCategory(Weather weather) {
         Map<String, List<ClothingCategory>> recommendations = new LinkedHashMap<>();
 
-        // 상의, 바지, 아우터, 기타 카테고리 초기화
+        // 카테고리 초기화
         recommendations.put("상의", new ArrayList<>());
         recommendations.put("바지", new ArrayList<>());
         recommendations.put("아우터", new ArrayList<>());
         recommendations.put("기타", new ArrayList<>());
 
-        double temp = weather.getT1h();
+        int t1h = weather.getT1h();
+        TemperatureRange temperatureRange = TemperatureRange.getRange(t1h);
 
-        // 28도 이상
-        if(temp >= 28){
-            // 상의
-            addClothingCategory(recommendations, "상의", "001001"); // 반소매 티셔츠
-            addClothingCategory(recommendations, "상의", "001011"); // 민소매 티셔츠
-
-            // 바지
-            addClothingCategory(recommendations, "바지", "003009"); // 숏 팬츠
-
-            // 원피스/스커트
-            addClothingCategory(recommendations, "기타", "100001"); // 미니원피스
-            addClothingCategory(recommendations, "기타", "100002"); // 미디원피스
-            addClothingCategory(recommendations, "기타", "100004"); // 미니스커트
-            addClothingCategory(recommendations, "기타", "100005"); // 미디스커트
-        }
-        // 23도 ~ 27도
-        else if (temp >= 23) {
-            // 상의
-            addClothingCategory(recommendations, "상의", "001001"); // 반소매 티셔츠
-            addClothingCategory(recommendations, "상의", "001011"); // 민소매 티셔츠
-
-            // 바지
-            addClothingCategory(recommendations, "바지", "003002"); // 데님 팬츠
-            addClothingCategory(recommendations, "바지", "003007"); // 코튼 팬츠
-            addClothingCategory(recommendations, "바지", "003008"); // 슈트 팬츠/슬랙스
-            addClothingCategory(recommendations, "바지", "003009"); // 숏 팬츠
-
-            // 원피스/스커트
-            addClothingCategory(recommendations, "기타", "100001"); // 미니원피스
-            addClothingCategory(recommendations, "기타", "100002"); // 미디원피스
-            addClothingCategory(recommendations, "기타", "100003"); // 맥시원피스
-            addClothingCategory(recommendations, "기타", "100004"); // 미니스커트
-            addClothingCategory(recommendations, "기타", "100005"); // 미디스커트
-            addClothingCategory(recommendations, "기타", "100006"); // 롱스커트
-
-        // 20도 ~ 22도
-        } else if (temp >= 20) {
-            addClothingCategory(recommendations, "아우터", "002022"); // 후드 집업
-            addClothingCategory(recommendations, "아우터", "002006"); // 나일론/코치 재킷
-
-            // 상의
-            addClothingCategory(recommendations, "상의", "001001"); // 반소매 티셔츠
-            addClothingCategory(recommendations, "상의", "001003"); // 피케/카라 티셔츠
-            addClothingCategory(recommendations, "상의", "001002"); // 셔츠 블라우스
-            addClothingCategory(recommendations, "상의", "001010"); // 긴소매 티셔츠
-
-            // 바지
-            addClothingCategory(recommendations, "바지", "003002"); // 데님 팬츠
-            addClothingCategory(recommendations, "바지", "003007"); // 코튼 팬츠
-            addClothingCategory(recommendations, "바지", "003008"); // 슈트 팬츠/슬랙스
-
-        // 16도 ~ 19도
-        } else if (temp >= 16) {
-            // 아우터
-            addClothingCategory(recommendations, "아우터", "002021"); // 카디건
-            addClothingCategory(recommendations, "아우터", "002021"); // 베스트
-            addClothingCategory(recommendations, "아우터", "002006"); // 나일론/코치재킷
-            addClothingCategory(recommendations, "아우터", "002002"); // 라이더스 재킷
-            addClothingCategory(recommendations, "아우터", "002004"); // 스타디움 재킷
-            addClothingCategory(recommendations, "아우터", "002017"); // 트러커 재킷
-            addClothingCategory(recommendations, "아우터", "002022"); // 후드 집업
-
-            // 상의
-            addClothingCategory(recommendations, "상의", "001005"); // 맨투맨/스웨트
-            addClothingCategory(recommendations, "상의", "001004"); // 후드/티셔츠
-            addClothingCategory(recommendations, "상의", "001006"); // 니트/스웨터
-            addClothingCategory(recommendations, "상의", "001010"); // 긴소매 티셔츠
-
-            // 바지
-            addClothingCategory(recommendations, "바지", "003002"); // 데님 팬츠
-            addClothingCategory(recommendations, "바지", "003007"); // 코튼 팬츠
-            addClothingCategory(recommendations, "바지", "003008"); // 슈트 팬츠/슬랙스
-
-
-        // 11도 ~ 15도
-        } else if (temp >= 11) {
-            // 아우터
-            addClothingCategory(recommendations, "아우터", "002002"); // 라이더스 재킷
-            addClothingCategory(recommendations, "아우터", "002004"); // 스타디움 재킷
-            addClothingCategory(recommendations, "아우터", "002017"); // 트러커 재킷
-            addClothingCategory(recommendations, "아우터", "002022"); // 후드 집업
-            addClothingCategory(recommendations, "아우터", "002014"); // 사파리/헌팅 재킷
-            addClothingCategory(recommendations, "아우터", "002008"); // 환절기 코트
-
-            // 상의
-            addClothingCategory(recommendations, "상의", "001005"); // 맨투맨/스웨트
-            addClothingCategory(recommendations, "상의", "001002"); // 셔츠/블라우스
-            addClothingCategory(recommendations, "상의", "001004"); // 후드/티셔츠
-            addClothingCategory(recommendations, "상의", "001006"); // 니트/스웨터
-            addClothingCategory(recommendations, "상의", "001010"); // 긴소매 티셔츠
-
-            // 바지
-            addClothingCategory(recommendations, "바지", "003002"); // 데님 팬츠
-            addClothingCategory(recommendations, "바지", "003007"); // 코튼 팬츠
-            addClothingCategory(recommendations, "바지", "003008"); // 슈트 팬츠/슬랙스
-
-
-        // 5도 ~ 10도
-        } else if (temp >= 5) {
-            // 아우터
-            addClothingCategory(recommendations, "아우터", "002023"); // 플리스/뽀글이
-            addClothingCategory(recommendations, "아우터", "002007"); // 겨울 싱글 코트
-            addClothingCategory(recommendations, "아우터", "002024"); // 겨울 더블 코트
-            addClothingCategory(recommendations, "아우터", "002013"); // 롱패딩/헤비 아우터
-            addClothingCategory(recommendations, "아우터", "002012"); // 숏패딩/헤비 아우터
-            addClothingCategory(recommendations, "아우터", "002016"); // 패딩 베스트
-
-            // 상의
-            addClothingCategory(recommendations, "상의", "001004"); // 후드/티셔츠
-            addClothingCategory(recommendations, "상의", "001006"); // 니트/스웨터
-            addClothingCategory(recommendations, "상의", "001005"); // 맨투맨/스웨트
-
-            // 바지
-            addClothingCategory(recommendations, "바지", "003002"); // 데님 팬츠
-            addClothingCategory(recommendations, "바지", "003007"); // 코튼 팬츠
-            addClothingCategory(recommendations, "바지", "003008"); // 슈트 팬츠/슬랙스
-
-            // 기타
-            addClothingCategory(recommendations, "기타", "101008"); // 머플러
-
-        // 4도 이하
-        } else  {
-            // 아우터
-            addClothingCategory(recommendations, "아우터", "002023"); // 플리스/뽀글이
-            addClothingCategory(recommendations, "아우터", "002007"); // 겨울 싱글 코트
-            addClothingCategory(recommendations, "아우터", "002024"); // 겨울 더블 코트
-            addClothingCategory(recommendations, "아우터", "002013"); // 롱패딩/헤비 아우터
-            addClothingCategory(recommendations, "아우터", "002012"); // 숏패딩/헤비 아우터
-            addClothingCategory(recommendations, "아우터", "002016"); // 패딩 베스트
-
-            // 상의
-            addClothingCategory(recommendations, "상의", "001004"); // 후드/티셔츠
-            addClothingCategory(recommendations, "상의", "001006"); // 니트/스웨터
-            addClothingCategory(recommendations, "상의", "001005"); // 맨투맨/스웨트
-
-            // 바지
-            addClothingCategory(recommendations, "바지", "003002"); // 데님 팬츠
-            addClothingCategory(recommendations, "바지", "003007"); // 코튼 팬츠
-            addClothingCategory(recommendations, "바지", "003008"); // 슈트 팬츠/슬랙스
-
-            // 기타
-            addClothingCategory(recommendations, "기타", "101008"); // 머플러
-            addClothingCategory(recommendations, "기타", "101004007"); // 장갑
-
+        switch (temperatureRange) {
+            case VERY_HOT:
+                addVeryHotClothing(recommendations);
+                break;
+            case HOT:
+                addHotClothing(recommendations);
+                break;
+            case WARM:
+                addWarmClothing(recommendations);
+                break;
+            case MILD:
+                addMildClothing(recommendations);
+                break;
+            case COOL:
+                addCoolClothing(recommendations);
+                break;
+            case COLD:
+                addColdClothing(recommendations);
+                break;
+            case VERY_COLD:
+                addVeryColdClothing(recommendations);
+                break;
         }
         recommendations.entrySet().removeIf(entry -> entry.getValue().isEmpty());
 
         return recommendations;
     }
-
     private void addClothingCategory(Map<String, List<ClothingCategory>> recommendations,
                                      String category, String categoryId) {
         ClothingCategory clothingCategory = clothingCategoryRepository
@@ -197,6 +75,142 @@ public class ClothingCategoryService {
                 .orElseThrow(() -> new RuntimeException("Category not found: " + categoryId));
 
         recommendations.get(category).add(clothingCategory);
+    }
+
+    private void addVeryHotClothing(Map<String, List<ClothingCategory>> recommendations){
+        // 상의
+        addClothingCategory(recommendations, "상의", "001001"); // 반소매 티셔츠
+        addClothingCategory(recommendations, "상의", "001011"); // 민소매 티셔츠
+
+        // 바지
+        addClothingCategory(recommendations, "바지", "003009"); // 숏 팬츠
+
+        // 원피스/스커트
+        addClothingCategory(recommendations, "기타", "100001"); // 미니원피스
+        addClothingCategory(recommendations, "기타", "100002"); // 미디원피스
+        addClothingCategory(recommendations, "기타", "100004"); // 미니스커트
+        addClothingCategory(recommendations, "기타", "100005"); // 미디스커트
+    }
+    private void addHotClothing(Map<String, List<ClothingCategory>> recommendations){
+        // 상의
+        addClothingCategory(recommendations, "상의", "001001"); // 반소매 티셔츠
+        addClothingCategory(recommendations, "상의", "001011"); // 민소매 티셔츠
+
+        // 바지
+        addClothingCategory(recommendations, "바지", "003002"); // 데님 팬츠
+        addClothingCategory(recommendations, "바지", "003007"); // 코튼 팬츠
+        addClothingCategory(recommendations, "바지", "003008"); // 슈트 팬츠/슬랙스
+        addClothingCategory(recommendations, "바지", "003009"); // 숏 팬츠
+
+        // 원피스/스커트
+        addClothingCategory(recommendations, "원피스/스커트", "100001"); // 미니원피스
+        addClothingCategory(recommendations, "원피스/스커트", "100002"); // 미디원피스
+        addClothingCategory(recommendations, "원피스/스커트", "100003"); // 맥시원피스
+        addClothingCategory(recommendations, "원피스/스커트", "100004"); // 미니스커트
+        addClothingCategory(recommendations, "원피스/스커트", "100005"); // 미디스커트
+        addClothingCategory(recommendations, "원피스/스커트", "100006"); // 롱스커트
+    }
+    private void addWarmClothing(Map<String, List<ClothingCategory>> recommendations){
+        addClothingCategory(recommendations, "아우터", "002022"); // 후드 집업
+        addClothingCategory(recommendations, "아우터", "002006"); // 나일론/코치 재킷
+
+        // 상의
+        addClothingCategory(recommendations, "상의", "001001"); // 반소매 티셔츠
+        addClothingCategory(recommendations, "상의", "001003"); // 피케/카라 티셔츠
+        addClothingCategory(recommendations, "상의", "001002"); // 셔츠 블라우스
+        addClothingCategory(recommendations, "상의", "001010"); // 긴소매 티셔츠
+
+        // 바지
+        addClothingCategory(recommendations, "바지", "003002"); // 데님 팬츠
+        addClothingCategory(recommendations, "바지", "003007"); // 코튼 팬츠
+        addClothingCategory(recommendations, "바지", "003008"); // 슈트 팬츠/슬랙스
+    }
+    private void addMildClothing(Map<String, List<ClothingCategory>> recommendations){
+        // 아우터
+        addClothingCategory(recommendations, "아우터", "002021"); // 카디건
+        addClothingCategory(recommendations, "아우터", "002021"); // 베스트
+        addClothingCategory(recommendations, "아우터", "002006"); // 나일론/코치재킷
+        addClothingCategory(recommendations, "아우터", "002002"); // 라이더스 재킷
+        addClothingCategory(recommendations, "아우터", "002004"); // 스타디움 재킷
+        addClothingCategory(recommendations, "아우터", "002017"); // 트러커 재킷
+        addClothingCategory(recommendations, "아우터", "002022"); // 후드 집업
+
+        // 상의
+        addClothingCategory(recommendations, "상의", "001005"); // 맨투맨/스웨트
+        addClothingCategory(recommendations, "상의", "001004"); // 후드/티셔츠
+        addClothingCategory(recommendations, "상의", "001006"); // 니트/스웨터
+        addClothingCategory(recommendations, "상의", "001010"); // 긴소매 티셔츠
+
+        // 바지
+        addClothingCategory(recommendations, "바지", "003002"); // 데님 팬츠
+        addClothingCategory(recommendations, "바지", "003007"); // 코튼 팬츠
+        addClothingCategory(recommendations, "바지", "003008"); // 슈트 팬츠/슬랙스
+    }
+    private void addCoolClothing(Map<String, List<ClothingCategory>> recommendations){
+        // 아우터
+        addClothingCategory(recommendations, "아우터", "002002"); // 라이더스 재킷
+        addClothingCategory(recommendations, "아우터", "002004"); // 스타디움 재킷
+        addClothingCategory(recommendations, "아우터", "002017"); // 트러커 재킷
+        addClothingCategory(recommendations, "아우터", "002022"); // 후드 집업
+        addClothingCategory(recommendations, "아우터", "002014"); // 사파리/헌팅 재킷
+        addClothingCategory(recommendations, "아우터", "002008"); // 환절기 코트
+
+        // 상의
+        addClothingCategory(recommendations, "상의", "001005"); // 맨투맨/스웨트
+        addClothingCategory(recommendations, "상의", "001002"); // 셔츠/블라우스
+        addClothingCategory(recommendations, "상의", "001004"); // 후드/티셔츠
+        addClothingCategory(recommendations, "상의", "001006"); // 니트/스웨터
+        addClothingCategory(recommendations, "상의", "001010"); // 긴소매 티셔츠
+
+        // 바지
+        addClothingCategory(recommendations, "바지", "003002"); // 데님 팬츠
+        addClothingCategory(recommendations, "바지", "003007"); // 코튼 팬츠
+        addClothingCategory(recommendations, "바지", "003008"); // 슈트 팬츠/슬랙스
+    }
+    private void addColdClothing(Map<String, List<ClothingCategory>> recommendations){
+        // 아우터
+        addClothingCategory(recommendations, "아우터", "002023"); // 플리스/뽀글이
+        addClothingCategory(recommendations, "아우터", "002007"); // 겨울 싱글 코트
+        addClothingCategory(recommendations, "아우터", "002024"); // 겨울 더블 코트
+        addClothingCategory(recommendations, "아우터", "002013"); // 롱패딩/헤비 아우터
+        addClothingCategory(recommendations, "아우터", "002012"); // 숏패딩/헤비 아우터
+        addClothingCategory(recommendations, "아우터", "002016"); // 패딩 베스트
+
+        // 상의
+        addClothingCategory(recommendations, "상의", "001004"); // 후드/티셔츠
+        addClothingCategory(recommendations, "상의", "001006"); // 니트/스웨터
+        addClothingCategory(recommendations, "상의", "001005"); // 맨투맨/스웨트
+
+        // 바지
+        addClothingCategory(recommendations, "바지", "003002"); // 데님 팬츠
+        addClothingCategory(recommendations, "바지", "003007"); // 코튼 팬츠
+        addClothingCategory(recommendations, "바지", "003008"); // 슈트 팬츠/슬랙스
+
+        // 기타
+        addClothingCategory(recommendations, "기타", "101008"); // 머플러
+    }
+    private void addVeryColdClothing(Map<String, List<ClothingCategory>> recommendations){
+        // 아우터
+        addClothingCategory(recommendations, "아우터", "002023"); // 플리스/뽀글이
+        addClothingCategory(recommendations, "아우터", "002007"); // 겨울 싱글 코트
+        addClothingCategory(recommendations, "아우터", "002024"); // 겨울 더블 코트
+        addClothingCategory(recommendations, "아우터", "002013"); // 롱패딩/헤비 아우터
+        addClothingCategory(recommendations, "아우터", "002012"); // 숏패딩/헤비 아우터
+        addClothingCategory(recommendations, "아우터", "002016"); // 패딩 베스트
+
+        // 상의
+        addClothingCategory(recommendations, "상의", "001004"); // 후드/티셔츠
+        addClothingCategory(recommendations, "상의", "001006"); // 니트/스웨터
+        addClothingCategory(recommendations, "상의", "001005"); // 맨투맨/스웨트
+
+        // 바지
+        addClothingCategory(recommendations, "바지", "003002"); // 데님 팬츠
+        addClothingCategory(recommendations, "바지", "003007"); // 코튼 팬츠
+        addClothingCategory(recommendations, "바지", "003008"); // 슈트 팬츠/슬랙스
+
+        // 기타
+        addClothingCategory(recommendations, "기타", "101008"); // 머플러
+        addClothingCategory(recommendations, "기타", "101004007"); // 장갑
     }
 
     @Transactional
@@ -238,7 +252,7 @@ public class ClothingCategoryService {
 
             // 각 카테고리별로 상품 조회 및 정렬
             for (ClothingCategory category : categories) {
-                // 해당 카테고리의 상품들을 likes 내림차순으로 최대 10개 조회
+                // 해당 카테고리의 상품들을 likes 내림차순으로 최대 12개 조회
                 List<ClothingProduct> products = clothingProductRepository
                         .findByCategoryId(category.getCategoryId())
                         .stream()
