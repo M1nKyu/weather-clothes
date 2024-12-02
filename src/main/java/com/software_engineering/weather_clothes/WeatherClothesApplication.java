458ae459f7760f8fbfca5dba1cbe7af3a14c2a75
eokjae <cjdeh0904@naver.com>
2024-12-02 13:38:32 +0900

    fix: Serviceworker

--- src/main/java/com/software_engineering/weather_clothes/WeatherClothesApplication.java
+++ src/main/java/com/software_engineering/weather_clothes/WeatherClothesApplication.java
@@ -8,11 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WeatherClothesApplication {
 
         public static void main(String[] args) {
-                Dotenv dotenv = Dotenv.load();  // .env 파일 로드
-                // 환경 변수를 시스템에 등록
-                dotenv.entries().forEach(entry ->
-                        System.setProperty(entry.getKey(), entry.getValue())
-                );
 
                 SpringApplication.run(WeatherClothesApplication.class, args);
         }
