f213704823bf9e1504f9f7c897d10279131b7575
M1nKyu <124333357+M1nKyu@users.noreply.github.com>
2024-12-02 15:00:39 +0900

    chore: Update WeatherClothesApplication.java

--- src/main/java/com/software_engineering/weather_clothes/WeatherClothesApplication.java
+++ src/main/java/com/software_engineering/weather_clothes/WeatherClothesApplication.java
@@ -13,12 +13,12 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class WeatherClothesApplication {
 
         public static void main(String[] args) {
-                Dotenv dotenv = Dotenv.load();  // .env 파일 로드
+                Dotenv dotenv = Dotenv.configure()
+                        .directory("/home/ubuntu/weather-clothes")  // .env 파일이 있는 디렉토리 경로를 지정
+                        .load();
                 // 환경 변수를 시스템에 등록
                 dotenv.entries().forEach(entry ->
                         System.setProperty(entry.getKey(), entry.getValue())
                 );
-
-                SpringApplication.run(WeatherClothesApplication.class, args);
         }
 }
